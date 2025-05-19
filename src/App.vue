436bdfb795d1c6a0c61888c5eb4af071a679c39d
Kevin Rousselet <krous74@gmail.com>
2025-05-19 14:43:33 +0200

    Dark Mode component added, responsive tweaked

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,59 @@
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+  <DarkModeToggle />
 </template>
 
-<style lang="scss">
+<script>
+import DarkModeToggle from "./components/DarkModeToggle.vue";
 
-*, ::before, ::after {
+export default {
+  components: {
+    DarkModeToggle,
+  },
+};
+</script>
+
+<style lang="scss">
+*,
+::before,
+::after {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+}
+
+body {
+  background-color: white;
+}
+
+body.dark-mode {
+  background-color: #121212;
+  color: #ffffff;
+}
+
+body.dark-mode a {
+  color: #9ae3ff;
+}
+
+.flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@keyframes stretch {
+  0% {
+    letter-spacing: 6px;
+  }
+
+  50% {
+    letter-spacing: 3vw;
+  }
+  100% {
+    letter-spacing: 6px;
+  }
 }
 
 @keyframes appear {
@@ -26,35 +70,53 @@
   }
 }
 
+.stretch {
+  animation: stretch 4s 0.2s ease-in-out forwards;
+}
+
 .appear {
-  animation: appear 3s .1s ease-in-out forwards;
+  animation: appear 3s 0.1s ease-in-out forwards;
 }
 
 .fromLeftToRight {
   position: relative;
   left: -100vw;
-  animation: fromLeftToRight 2s .2s ease-in-out forwards
+  animation: fromLeftToRight 2s 0.2s ease-in-out forwards;
 }
-
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
 
 nav {
   display: flex;
   justify-content: space-around;
   align-items: center;
+  margin: 0 auto;
+  width: 100%;
+  @media (min-width: 320px) and (max-width: 550px) {
+    height: 80px;
+  }
+  @media (min-width: 551px) {
+    height: 120px;
+  }
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    @media (min-width: 320px) and (max-width: 550px) {
+      letter-spacing: 6px;
+      font-size: 3vw;
+    }
+    @media (min-width: 991px) {
+      letter-spacing: 8px;
+      font-size: 3vw;
+    }
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: red;
     }
   }
 }
--- src/components/VideoMaskComponent.vue
+++ src/components/VideoMaskComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <section>
+    <section class="mask-airfrance">
       <div class="video-container wrapper appear fromLeftToRight">
         <video autoplay muted loop playsinline preload>
           <source src="/videos/sky.mp4" type="video/mp4" />
@@ -11,7 +11,14 @@
         <svg preserveAspectRatio="xMidYMid slice" viewBox="0 0 285 80">
           <defs>
             <mask id="mask" x="0" y="0" width="100%" height="100%">
-              <rect x="0" y="0" width="100%" height="100%" fill="white" class="appear"/>
+              <rect
+                x="0"
+                y="0"
+                width="100%"
+                height="100%"
+                fill="white"
+                class="appear"
+              />
               <text
                 x="50%"
                 y="50%"
@@ -24,32 +31,57 @@
             </mask>
           </defs>
           <rect
+            id="text-background"
             x="0"
             y="0"
             width="100%"
             height="100%"
-            fill="white"
+            fill="var(--mask-fill)"
             mask="url(#mask)"
           />
         </svg>
       </div>
     </section>
-    <section></section>
+    <section class="slogan">
+      <div class="flex appear">
+        <h2 class="stretch">Viser l'excellence</h2>
+      </div>
+    </section>
   </main>
 </template>
 
 <style scoped lang="scss">
+// CHANGER TO BLACK AVEC LE DARKMODE ET LE BODY DE APP EN BLACK AUSSI
+#text-background {
+  --mask-fill: white;
+}
+
+body.dark-mode #text-background {
+  --mask-fill: black;
+}
 main {
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 1fr;
   gap: 50px;
 
-  section {
+  .mask-airfrance {
     position: relative;
     margin: 0 auto;
     width: 100%;
-    height: 300px;
+    height: 150px;
+    @media (min-width: 320px) and (max-width: 991px) {
+      margin-top: 150px;
+    }
+    @media (min-width: 992px) and (max-width: 2440px) {
+      margin-top: 150px;
+    }
+    @media (min-width: 1921px) and (max-width: 2440px) {
+      margin-top: 180px;
+    }
+    @media (min-width: 2441px) {
+      margin-top: 210px;
+    }
 
     .wrapper {
       width: 100%;
@@ -86,25 +118,20 @@ main {
             display: flex;
             justify-self: center;
             align-self: center;
-            @media (min-width: 992px) and (max-width: 2440px) {
-              height: 400px;
-              max-height: 400px;
-            }
-            @media (min-width: 2441px) and (max-width: 3840px) {
-                  height: 800px;
-                  max-height: 800px;
-            }
           }
 
           text {
             @media (min-width: 992px) and (max-width: 1920px) {
-              font-size: 2.5vw;
+              font-size: 1.6vw;
+              letter-spacing: 5px;
             }
-             @media (min-width: 1921px) and (max-width: 2440px) {
-              font-size: 2vw;
+            @media (min-width: 1921px) and (max-width: 2440px) {
+              font-size: 1vw;
+              letter-spacing: 5px;
             }
             @media (min-width: 2441px) {
-              font-size: 1vw;
+              font-size: 0.8vw;
+              letter-spacing: 10px;
             }
             font-size: 4vw;
             font-weight: 900;
@@ -114,11 +141,20 @@ main {
       }
     }
   }
-}
-
-@media (min-width: 320px) and (max-width: 550px) {
-  main {
-    height: 800px;
+  .slogan {
+    div {
+      color: #002157;
+      @media (min-width: 320px) and (max-width: 550px) {
+        letter-spacing: 6px;
+        font-size: 3vw;
+      }
+      @media (min-width: 991px) {
+        letter-spacing: 8px;
+        font-size: 3.6vw;
+        margin-top: 150px;
+      }
+      opacity: 0;
+    }
   }
 }
 </style>
