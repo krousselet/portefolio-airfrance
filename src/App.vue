38d9e766e9f4291ee06f6d7870dda90f19cce03f
Kevin Rousselet <krous74@gmail.com>
2025-05-19 11:42:36 +0200

    air france animation mask all responsive

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+*, ::before, ::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
+@keyframes appear {
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes fromLeftToRight {
+  100% {
+    left: 0;
+  }
+}
+
+.appear {
+  animation: appear 3s .1s ease-in-out forwards;
+}
+
+.fromLeftToRight {
+  position: relative;
+  left: -100vw;
+  animation: fromLeftToRight 2s .2s ease-in-out forwards
+}
+
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
 nav {
-  padding: 30px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 
   a {
     font-weight: bold;
---
+++ src/components/VideoMaskComponent.vue
@@ -0,0 +1,124 @@
+<template>
+  <main>
+    <section>
+      <div class="video-container wrapper appear fromLeftToRight">
+        <video autoplay muted loop playsinline preload>
+          <source src="/videos/sky.mp4" type="video/mp4" />
+          Le navigateur ne supporte pas la vidéo.
+        </video>
+      </div>
+      <div class="svg-container">
+        <svg preserveAspectRatio="xMidYMid slice" viewBox="0 0 285 80">
+          <defs>
+            <mask id="mask" x="0" y="0" width="100%" height="100%">
+              <rect x="0" y="0" width="100%" height="100%" fill="white" class="appear"/>
+              <text
+                x="50%"
+                y="50%"
+                dominant-baseline="middle"
+                text-anchor="middle"
+                fill="black"
+              >
+                AIR FRANCE
+              </text>
+            </mask>
+          </defs>
+          <rect
+            x="0"
+            y="0"
+            width="100%"
+            height="100%"
+            fill="white"
+            mask="url(#mask)"
+          />
+        </svg>
+      </div>
+    </section>
+    <section></section>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  gap: 50px;
+
+  section {
+    position: relative;
+    margin: 0 auto;
+    width: 100%;
+    height: 300px;
+
+    .wrapper {
+      width: 100%;
+      height: 100%;
+
+      video {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        position: absolute;
+        z-index: -1;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    }
+
+    .svg-container {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 100;
+
+      svg {
+        width: 100%;
+        height: 100%;
+
+        mask {
+          width: 100%;
+          max-height: 800px;
+          rect {
+            display: flex;
+            justify-self: center;
+            align-self: center;
+            @media (min-width: 992px) and (max-width: 2440px) {
+              height: 400px;
+              max-height: 400px;
+            }
+            @media (min-width: 2441px) and (max-width: 3840px) {
+                  height: 800px;
+                  max-height: 800px;
+            }
+          }
+
+          text {
+            @media (min-width: 992px) and (max-width: 1920px) {
+              font-size: 2.5vw;
+            }
+             @media (min-width: 1921px) and (max-width: 2440px) {
+              font-size: 2vw;
+            }
+            @media (min-width: 2441px) {
+              font-size: 1vw;
+            }
+            font-size: 4vw;
+            font-weight: 900;
+            font-family: "Courier New", Courier, monospace;
+          }
+        }
+      }
+    }
+  }
+}
+
+@media (min-width: 320px) and (max-width: 550px) {
+  main {
+    height: 800px;
+  }
+}
+</style>
